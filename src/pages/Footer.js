d08c48d6b41ee23f03e2f487ca6b0003896e3fdf
Raffi Fauzan <111486276+raffifauzan@users.noreply.github.com>
2022-08-29 17:10:04 +0700

    main

---
+++ src/pages/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+              <b><a href="https://indrijunanda.gitlab.io/" target="_blank">indrijunanda</a></b>
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
